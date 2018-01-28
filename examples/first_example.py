14b3c005a82fa45b346f55b01417f8fddd92e918
wardbradt <bbradt@andover.edu>
2018-01-28 16:23:28 -0500

    shortened example

--- examples/first_example.py
+++ examples/first_example.py
@@ -2,7 +2,6 @@ from HTMLSentenceTokenizer import HTMLSentenceTokenizer
 
 example_html_one = open('example_html_one.html', 'r').read()
 
-tokenizer = HTMLSentenceTokenizer()
-parsed_sentences = tokenizer.feed(example_html_one)
+parsed_sentences = HTMLSentenceTokenizer().feed(example_html_one)
 
 print(parsed_sentences)
